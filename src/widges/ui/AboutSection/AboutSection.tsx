118d3204179443725d3b4bf21b9b582a6f2421f8
Anastasiya Poleshuk <anastasiyapaliashuk@gmail.com>
2024-09-26 20:51:48 +0300

    feat: add styles

--- src/widges/ui/AboutSection/AboutSection.tsx
+++ src/widges/ui/AboutSection/AboutSection.tsx
@@ -2,11 +2,6 @@ import { AboutItem } from "src/entities/index";
 import { HeartOutlined, SmileOutlined, ToolOutlined } from "@ant-design/icons";
 import "./AboutSection.scss";
 
-const iconStyle = {
-  fontSize: 70,
-  color: "#fafafa",
-};
-
 export const AboutSection = () => {
   return (
     <section className="about__section">
@@ -16,17 +11,17 @@ export const AboutSection = () => {
           <AboutItem
             title="Индивидаульный подход"
             description="Мастер учитывает все пожелания клиентов и подстраивается под ваш график."
-            icon={<ToolOutlined style={{ ...iconStyle }} />}
+            icon={<ToolOutlined className="about__icon" />}
           />
           <AboutItem
             title="Опытный и вежливый мастер"
             description="Мастер обладает большим опытом и профессионализмом, всегда вежлив и готовы объяснить каждый этап задачи."
-            icon={<HeartOutlined style={{ ...iconStyle }} />}
+            icon={<HeartOutlined className="about__icon" />}
           />
           <AboutItem
             title="Прозрачные цены"
             description="Никаких скрытых платежей — вы всегда знаете, за что платите."
-            icon={<SmileOutlined style={{ ...iconStyle }} />}
+            icon={<SmileOutlined className="about__icon" />}
           />
         </div>
       </div>
